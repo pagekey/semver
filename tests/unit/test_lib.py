a5dc808bb49a23566ef9ff74f45d05fe11ffa9a6
Stephen Grice <stephengrice@live.com>
2024-07-08 00:54:11 -0400

    Add missing commas

--- tests/unit/test_lib.py
+++ tests/unit/test_lib.py
@@ -185,8 +185,8 @@ def test_apply_tag_with_new_tag_tags_and_pushes(mock_system):
 
     # Assert.
     commands = [
-        f'sed "s/^version = \\"[0-9]\\+\\.[0-9]\\+\\.[0-9]\\"/version = \\"{new_tag_stripped}\\"/" cargo.toml'
-        f'sed -i "s/\\"version\\": \\"[0-9]\\+\\.[0-9]\\+\\.[0-9]\\"/\\"version\\": \\"{new_tag_stripped}\\"/" package.json'
+        f'sed "s/^version = \\"[0-9]\\+\\.[0-9]\\+\\.[0-9]\\"/version = \\"{new_tag_stripped}\\"/" cargo.toml',
+        f'sed -i "s/\\"version\\": \\"[0-9]\\+\\.[0-9]\\+\\.[0-9]\\"/\\"version\\": \\"{new_tag_stripped}\\"/" package.json',
         f"git config --global user.email semver@pagekey.io",
         f'git config --global user.name "PageKey Semver"',
         f"git add --all",
