4cacb60f2e9303f987d2a126ffabc9d80e1bb349
PageKey Semver <semver@pagekey.io>
2024-07-24 08:51:51 -0400

    Add basic pydantic class for config

--- src/pagekey_semver/config.py
+++ src/pagekey_semver/config.py
@@ -1,11 +1,24 @@
 """Module related to config files."""
-import os
+import json
 from pathlib import Path
+from typing import List
+
+from pydantic import BaseModel
 
 
-DEFAULT_CONFIG = "coming soon"
+DEFAULT_CONFIG_DICT = {
+    "prefixes": ["something"],
+}
+
+class SemverConfig(BaseModel):
+    prefixes: List[str]
 
 
 def load_config(config_path: Path) -> None:
     if not config_path.is_file():
-        return DEFAULT_CONFIG
+        return SemverConfig(**DEFAULT_CONFIG_DICT)
+    with open(config_path, "r") as config_file:
+        config_raw = config_file.read()
+    config_dict = json.loads(config_raw)
+    config_merged = {**DEFAULT_CONFIG_DICT, **config_dict}
+    return SemverConfig(**config_merged)
--- tests/unit/test_config.py
+++ tests/unit/test_config.py
@@ -4,14 +4,13 @@
 from pathlib import Path
 from unittest.mock import MagicMock, mock_open, patch
 
-from pagekey_semver.config import DEFAULT_CONFIG, load_config
+from pagekey_semver.config import DEFAULT_CONFIG_DICT, SemverConfig, load_config
 
 MODULE_UNDER_TEST = "pagekey_semver.config"
 
 
-@patch(f'{MODULE_UNDER_TEST}.os')
 @patch('builtins.open', new_callable=mock_open)
-def test_load_config_with_file_not_found_returns_default_config(mock_builtin_open, mock_os):
+def test_load_config_with_file_not_found_returns_default_config(mock_builtin_open):
     # Arrange.
     mock_path = MagicMock()
     mock_path.is_file.return_value = False
@@ -22,24 +21,22 @@ def test_load_config_with_file_not_found_returns_default_config(mock_builtin_ope
     # Assert.
     mock_path.is_file.assert_called()
     mock_builtin_open.assert_not_called()
-    assert config == DEFAULT_CONFIG
+    assert config == SemverConfig(**DEFAULT_CONFIG_DICT)
 
 
+@patch('builtins.open', new_callable=mock_open)
+def test_load_config_with_existing_file_parses_and_merges_configs(mock_builtin_open):
+    # Arrange.
+    mock_path = MagicMock()
+    mock_path.is_file.return_value = True
+    mock_file = mock_builtin_open.return_value
+    mock_file.read.return_value = '{"prefixes": []}'
 
-# def test_parse_config
-    # # Arrange.
-    # mock_file = mock_open.return_value
+    # Act.
+    config = load_config(mock_path)
 
-    # # Act.
-    # config = load_config(".semver")
-
-    # # Assert.
-    # mock_open.assert_called_with(".semver", "r")
-    
-    # mock_file.write.assert_has_calls([
-    #     call(f"## {version}\n\n"),
-    #     call("- fix: Do something somewhat important (aaaaa2)\n"),
-    #     call("- feat: Add something (aaaaa3)\n"),
-    #     call("- major: Wow this is a big deal (aaaaa5)\n"),
-    #     call("\n"),
-    # ])
+    # Assert.
+    mock_path.is_file.assert_called()
+    mock_builtin_open.assert_called_with(mock_path, "r")
+    mock_file.read.assert_called()
+    assert len(config.prefixes) == 0
