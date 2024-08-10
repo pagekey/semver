cb0516f0fead14db0060c052e6acd29c06dcaaf3
integration tester <integration@test.com>
2024-08-10 13:04:38 -0400

    Test/implement perform_replace on SedReplaceFile

--- src/pagekey_semver/replace_file/sed.py
+++ src/pagekey_semver/replace_file/sed.py
@@ -2,6 +2,7 @@ from typing import Literal
 
 from pagekey_semver.models import Tag
 from pagekey_semver.replace_file.base import ReplaceFile, ReplaceFileType
+from pagekey_semver.util.command_runner import CommandRunner, SubprocessCommandRunner
 
 
 class SedReplaceFile(ReplaceFile):
@@ -10,9 +11,27 @@ class SedReplaceFile(ReplaceFile):
     type: Literal[ReplaceFileType.SED] = ReplaceFileType.SED
     script: str
 
-    def perform_replace(self, tag: Tag) -> str:
+    def perform_replace(self, tag: Tag, runner: CommandRunner = SubprocessCommandRunner()) -> str:
         """Run the sed program to replace a tag in a file.
+
+        Replaces %M/%m/%p with tag's major/minor/patch,
+        then calls sed with the provided `script`.
 
         Args:
             script: Sed script to run on the file.
         """
+        # Check if the sed executable is available.
+        result = runner.run("which sed", raise_on_command_fail=False)
+        if result.exit_code != 0:
+            raise EnvironmentError(
+                "Sed executable not found on system - have you installed sed?"
+            )
+        # Replace placeholders in script using tag's major/minor/patch.
+        script_escaped = self.script.replace("'", "\\'")
+        script_replaced = (
+            script_escaped.replace("%M", str(tag.major))
+            .replace("%m", str(tag.minor))
+            .replace("%p", str(tag.patch))
+        )
+        # Run sed.
+        runner.run(f"sed -i '{script_replaced}' {self.name}")
---
+++ tests/unit/replace_file/test_sed.py
@@ -0,0 +1,99 @@
+"""Module to test SED file replacer."""
+
+from unittest.mock import MagicMock, patch
+
+import pytest
+from pagekey_semver.models import Tag
+from pagekey_semver.replace_file.sed import SedReplaceFile
+from pagekey_semver.util.command_runner import CommandResult, CommandRunnerException
+
+
+MODULE_UNDER_TEST = "pagekey_semver.replace_file.sed"
+
+class TestSedReplaceFile:
+    class Test_perform_replace:
+        def test_with_sed_not_installed_raises_error(self):
+            # Arrange.
+            tag = Tag("v2.0.0", 2, 0, 0)
+            runner = MagicMock()
+            runner.run.return_value = CommandResult(
+                exit_code=1,
+                stdout="",
+                stderr="",
+            )
+            replacer = SedReplaceFile(
+                name="file.md",
+                script="s/something/other/g",
+            )
+
+            # Act, Assert.
+            with pytest.raises(EnvironmentError):
+                replacer.perform_replace(tag, runner)
+            
+            # Assert.
+            runner.run.assert_called_with("which sed", raise_on_command_fail=False)
+
+
+        @pytest.mark.parametrize(
+            "input_script, tag, expected_command",
+            [
+                (
+                    "s/something/else/g",
+                    Tag("v2.0.0", 2, 0, 0),
+                    "sed -i 's/something/else/g' file.md",
+                ),
+                (
+                    's/^version=".*"/version=%M.%m.%p/g',
+                    Tag("v2.0.0", 2, 0, 1),
+                    "sed -i 's/^version=\".*\"/version=2.0.1/g' file.md",
+                ),
+                (
+                    "s/^version='.*'/version=%M.%m.%p/g",
+                    Tag("v2.0.0", 2, 0, 1),
+                    "sed -i 's/^version=\\'.*\\'/version=2.0.1/g' file.md",
+                ),
+            ],
+        )
+        def test_with_script_calls_sed(
+            self,
+            input_script,
+            tag,
+            expected_command,
+        ):
+            # Arrange.
+            runner = MagicMock()
+            runner.run.side_effect = [
+                # which shutil
+                CommandResult(
+                    exit_code=0,
+                    stdout="",
+                    stderr="",
+                ),
+                # running sed
+                CommandResult(
+                    exit_code=0,
+                    stdout="",
+                    stderr="",
+                ),
+            ]
+            replacer = SedReplaceFile(
+                name="file.md",
+                script=input_script,
+            )
+
+            # Act.
+            replacer.perform_replace(tag, runner)
+
+            # Assert.
+            runner.run.assert_called_with(expected_command)
+
+        def test_with_failed_sed_raises_error(self):
+            # Arrange.
+            tag = Tag("v2.0.0", 2, 0, 0)
+            replacer = SedReplaceFile(
+                name="some_file.md", script="some_invalid_script"
+            )
+
+            # Act, Assert.
+            with pytest.raises(CommandRunnerException):
+                replacer.perform_replace(tag)
