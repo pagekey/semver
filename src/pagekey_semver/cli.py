261ec9410fc022914d999e1a19135f07ae9046a8
Stephen Grice <stephengrice@live.com>
2024-08-12 00:07:16 -0400

    fix: Add publish2 job

--- src/pagekey_semver/cli.py
+++ src/pagekey_semver/cli.py
@@ -72,6 +72,7 @@ def cli_entrypoint(args=sys.argv[1:]):
             # Set an env var for subsequent steps in GitHub Actions.
             with open(os.environ["GITHUB_ENV"], "w") as f:
                 f.write("SEMVER_NEW_RELEASE=true")
+                print("SEMVER_NEW_RELEASE=true")
         else:
             print(f"Would apply version {next_version.name}.", flush=True)
             print("Dry run mode - not applying version.", flush=True)
