7eeefe8ee24904aed6f77487ae42b87508a9681c
Stephen Grice <stephengrice@live.com>
2024-08-16 07:40:37 -0400

    Add part of test/impl for create_release

--- src/pagekey_semver/integrations/release_creator.py
+++ src/pagekey_semver/integrations/release_creator.py
@@ -1,7 +1,8 @@
 import abc
-import requests
+import os
 from typing import Optional
 
+import requests
 from pydantic import BaseModel
 
 from pagekey_semver.models import Tag
@@ -22,16 +23,37 @@ class ReleaseCreator(abc.ABC):
     def create_release(self, release_config: CreateReleaseConfig, tag: Tag):
         """."""
 
+
 class GitHubIntegrationConfig(BaseModel):
     """Configuration for any GitHub-specific integrations."""
 
     create_release: Optional[CreateReleaseConfig] = None
+
 
 class GitLabIntegrationConfig(BaseModel):
     """Configuration for any GitLab-specific integrations."""
 
     create_release: Optional[CreateReleaseConfig] = None
 
+
 class GitHubReleaseCreator(ReleaseCreator):
     def create_release(self, release_config: CreateReleaseConfig, tag: Tag):
-        pass
+        token = os.getenv("GITHUB_TOKEN")
+
+
+class GitLabReleaseCreator(ReleaseCreator):
+    def create_release(self, release_config: CreateReleaseConfig, tag: Tag):
+        token = os.getenv("GITLAB_TOKEN")
+        requests.post(
+            f"https://gitlab.com/api/v4/projects/{release_config.project}/releases",
+            json={
+                "tag_name": "",
+                "name": "",
+                "description": "",
+                "ref": "",
+            },
+            headers={
+                "PRIVATE-TOKEN": token,
+                "Content-Type": "application/json",
+            }
+        )
--- tests/unit/integrations/test_release_creator.py
+++ tests/unit/integrations/test_release_creator.py
@@ -2,7 +2,7 @@
 
 
 from unittest.mock import patch
-from pagekey_semver.integrations.release_creator import CreateReleaseConfig, GitHubReleaseCreator
+from pagekey_semver.integrations.release_creator import CreateReleaseConfig, GitHubReleaseCreator, GitLabReleaseCreator
 from pagekey_semver.models import Tag
 
 
@@ -12,8 +12,9 @@ MODULE_UNDER_TEST = "pagekey_semver.integrations.release_creator"
 class TestGitHubReleaseCreator:
     class Test_create_release:
 
+        @patch(f"{MODULE_UNDER_TEST}.os")
         @patch(f"{MODULE_UNDER_TEST}.requests")
-        def test_with_successful_request_makes_request(self, mock_requests):
+        def test_with_successful_request_makes_request(self, mock_requests, mock_os):
             # Arrange.
             config = CreateReleaseConfig(
                 project="me/project",
@@ -23,16 +24,61 @@ class TestGitHubReleaseCreator:
             )
             tag = Tag("v1.0.0", 1, 0, 0)
             creator = GitHubReleaseCreator()
+            mock_os.side_effect = ["my-github-token"]
             
             # Act.
             creator.create_release(config, tag)
             
             # Assert.
-            pass
+            mock_os.getenv.assert_called_with("GITHUB_TOKEN")
+            mock_requests.post.assert_called_with(
+                "https://api.github.com/repos/me/project/releases",
+                json={
+                    "tag_name": "v1.0.0",
+                    "name": "Version 1.0.0",
+                    "body": "Here is yet another release.",
+                    "draft": False,
+                    "prerelease": False,
+                },
+                headers={
+                    "Authorization": f"token my-github-token",
+                    "Accept": "application/bnd.github.v3+json",
+                }
+            )
 
 
 class TestGitLabReleaseCreator:
     class Test_create_release:
-        def test_with_successful_request_makes_request(self):
-            pass
 
+        @patch(f"{MODULE_UNDER_TEST}.os")
+        @patch(f"{MODULE_UNDER_TEST}.requests")
+        def test_with_successful_request_makes_request(self, mock_requests, mock_os):
+            # Arrange.
+            config = CreateReleaseConfig(
+                project="1234",
+                token_variable="GITLAB_TOKEN",
+                title_format="Version %M.%m.%p",
+                body="Here is yet another release."
+            )
+            tag = Tag("v1.0.0", 1, 0, 0)
+            creator = GitLabReleaseCreator()
+            mock_os.side_effect = ["my-gitlab-token"]
+            
+            # Act.
+            creator.create_release(config, tag)
+            
+            # Assert.
+            mock_os.getenv.assert_called_with("GITLAB_TOKEN")
+            mock_requests.post.assert_called_with(
+                "https://gitlab.com/api/v4/projects/1234/releases",
+                json={
+                    "tag_name": "v1.0.0",
+                    "name": "Version 1.0.0",
+                    "description": "Here is yet another release.",
+                    "ref": "v1.0.0",
+                },
+                headers={
+                    "PRIVATE-TOKEN": f"my-gitlab-token",
+                    "Content-Type": "application/json",
+                }
+            )
